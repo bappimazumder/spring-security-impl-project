4d912ce4e38b773e809cd6804f04ccd5bbfeca7c
Bappi Mazumder <mazumderbappi@gmail.com>
2023-05-11 13:45:44 +0600

    Set password enoder

--- src/main/java/com/example/springsecurityproject/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurityproject/security/ApplicationSecurityConfig.java
@@ -1,12 +1,25 @@
 package com.example.springsecurityproject.security;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     protected void configure(HttpSecurity httpSecurity) throws Exception{
        httpSecurity.authorizeRequests()
@@ -16,5 +29,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                .authenticated()
                .and()
                .httpBasic();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+      UserDetails userBappi =  User.builder()
+                .username("bappimazumder")
+                .password(passwordEncoder.encode("HelloSecurity"))
+                .roles("STUDENT")
+                .build();
+       return new InMemoryUserDetailsManager(userBappi);
     }
 }
