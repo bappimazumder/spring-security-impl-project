c60f16be18dfc57a286581845c1e358406a04bbb
Bappi Mazumder <mazumderbappi@gmail.com>
2023-05-11 13:05:39 +0600

    Initial commit

---
+++ src/main/java/com/example/springsecurityproject/security/ApplicationSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.springsecurityproject.security;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+       httpSecurity.authorizeRequests()
+               .antMatchers("/","index","/css/*","/js/*")
+               .permitAll()
+               .anyRequest()
+               .authenticated()
+               .and()
+               .httpBasic();
+    }
+}
---
+++ src/main/java/com/example/springsecurityproject/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.springsecurityproject.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENT_LIST = Arrays.asList(
+            new Student(1,"Bappi Mazumder"),
+            new Student(2,"Maria Bond"),
+            new Student(3,"David Balame")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId ){
+         return STUDENT_LIST.stream()
+        .filter(student -> studentId.equals(student.getStudentId())).
+                findFirst().orElseThrow(()-> new IllegalArgumentException("student" + studentId));
+    }
+}
