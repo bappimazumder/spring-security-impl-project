ab8b8d49395794202fa79c53b9b4a282017a9442
Bappi Mazumder <mazumderbappi@gmail.com>
2023-05-19 23:03:13 +0600

    add jwt authentication provider

---
+++ src/main/java/com/example/springsecurityproject/jwt/JwtUsernameAndPasswordAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package com.example.springsecurityproject.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtUsernameAndPasswordAuthenticationProvider extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtUsernameAndPasswordAuthenticationProvider(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            UsernamePasswordAuthenticationRequest authenticationRequest = new ObjectMapper().readValue(
+                    request.getInputStream(),UsernamePasswordAuthenticationRequest.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getUserName(),
+                    authenticationRequest.getPassword()
+            );
+            Authentication authenticate = authenticationManager.authenticate(authentication);
+            return authenticate;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
